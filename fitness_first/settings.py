e8388559214083e42f5198a8c2c4922ca6c237a8
phillrutherford <phillip.h.rutherford@gmail.com>
2021-03-29 15:33:19 +0000

    settings.py altered for deployment

--- fitness_first/settings.py
+++ fitness_first/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9v^i772vp7hivan@9g+^vt)zv9z)^0&0kaa3d$=11@8xvridt2'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['Phillip Rutherford-fitness-first1.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['phillrutherford-fitness-first1.herokuapp.com', 'localhost']
 
 
 # Application definition
